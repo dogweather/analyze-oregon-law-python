eace66284947c409475b556e6bcb058a9e8c1a68
Robb Shecter <robb@weblaws.org>
2016-08-27 21:38:15 -0700

    finished string ops

--- string_ops.py
+++ string_ops.py
@@ -1,6 +1,21 @@
+def clean_up(s):
+    return next(split_into_sentences(fix_hyphenation(fix_whitespace(s))))
+
+
 def fix_whitespace(s):
     return s.replace("\n", " ")
 
 
 def fix_hyphenation(s):
     return s.replace("- ", "")
+
+
+def split_into_sentences(s):
+    return map(ensure_ends_with_period, s.split(sep=". "))
+
+
+def ensure_ends_with_period(s):
+    if s.endswith("."):
+        return s
+    else:
+        return s + "."
--- string_ops_test.py
+++ string_ops_test.py
@@ -4,6 +4,9 @@ from string_ops import *
 def test_fix_whitespace():
     assert fix_whitespace("new\nline") == "new line"
 
-
 def test_fix_hyphenation():
     assert fix_hyphenation("auto- mobile") == "automobile"
+
+def test_clean_up():
+    input = "Relating to the state transient lodging tax; creating\nnew provisions; amending ORS 284.131 and\n320.305; prescribing an effective date; and pro-\nviding for revenue raising that requires approval\nby a three-fifths majority.\nWhereas Enrolled House Bill 2267 (chapter 818,"
+    assert clean_up(input) == "Relating to the state transient lodging tax; creating new provisions; amending ORS 284.131 and 320.305; prescribing an effective date; and providing for revenue raising that requires approval by a three-fifths majority."
